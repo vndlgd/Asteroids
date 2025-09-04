064a6846012c996eeaa1f5e100088de61f1c5c15
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2025-09-04 00:50:46 -0700

    finalize project

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import * 
+from asteroid import * 
+from asteroidfield import * 
+from shot import * 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time_clock = pygame.time.Clock()
+
+    fps = 60
+    dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        player.timer -= dt
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+               sys.exit("Game over!") 
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = time_clock.tick(fps) / 1000
+
 
 if __name__ == "__main__":
     main()
